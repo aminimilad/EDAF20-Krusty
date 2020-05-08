c760fb18f807254c4ca39c2e007df84bac1ed8cb
milad0823 <milad0823@gmail.com>
2020-05-08 04:24:54 +0200

    Not clean yet...

--- krusty/src/main/java/krusty/Database.java
+++ krusty/src/main/java/krusty/Database.java
@@ -3,6 +3,10 @@ package krusty;
 import spark.Request;
 import spark.Response;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
@@ -13,19 +17,35 @@ public class Database {
 	/**
 	 * Modify it to fit your environment and then use this string when connecting to your database!
 	 */
-	private static final String jdbcString = "jdbc:mysql://localhost/krusty";
-
-	// For use with MySQL or PostgreSQL
+	private static final String jdbcString = "jdbc:sqlite://C:/Users/milad/krusty/kDB.db";
+	private Connection conn;
+	/* For use with MySQL or PostgreSQL
 	private static final String jdbcUsername = "<CHANGE ME>";
-	private static final String jdbcPassword = "<CHANGE ME>";
+	private static final String jdbcPassword = "<CHANGE ME>"
+	*/
 
 	public void connect() {
 		// Connect to database here
+		try {
+			conn = DriverManager.getConnection(jdbcString);
+			System.out.println(conn);
+		}
+		catch(Exception err){
+			err.printStackTrace();
+		}
 	}
 
 	// TODO: Implement and change output in all methods below!
 
 	public String getCustomers(Request req, Response res) {
+		
+		String query = "SELECT * FROM Customers";
+
+		try (PreparedStatement statement = conn.prepareStatement(query)) {
+			return Jsonizer.toJson(statement.executeQuery(), "customers");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return "{}";
 	}
 
