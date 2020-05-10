74aad4a1659886f67aeb26427e7fa329a57f682e
milad0823 <milad0823@gmail.com>
2020-05-10 05:01:33 +0200

    Still unclean. CreatePallets done - 3 tests to go. Janitor is slow today

--- krusty/src/main/java/krusty/Database.java
+++ krusty/src/main/java/krusty/Database.java
@@ -8,7 +8,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -99,40 +102,41 @@ public class Database {
 
 	public String createPallet(Request req, Response res) {
 		
-		String cookie = req.queryParams("cookies");
+		String cookie = req.queryParams("cookie");
 		
 		if (!cookieExists(cookie)) {
 			return "{\"status\":\"unknown cookie\"}";
 		}
 		//Will be used to create a pallet ofc
 		int orderID = createOrder();
-		
+		int palletID = -1;
 		String Q = "INSERT INTO Pallets(cookieName, orderID, dateProduced, isBlocked, location) VALUES (?,?,?,?,?)";
-		
+		Calendar calendar = Calendar.getInstance();
+		Timestamp ourJavaTimestampObject = new Timestamp(calendar.getTime().getTime());
 		try(PreparedStatement stmt = conn.prepareStatement(Q)) {
 			
 			stmt.setString(1,  cookie);
 			stmt.setInt(2, orderID);
-			stmt.setString(3, "CURRENT_TIMESTAMP");
+			stmt.setString(3, ourJavaTimestampObject.toString());
 			stmt.setString(4, "no");
-			stmt.setString(4, "SELECT address FROM Orders, Customers WHERE Orders.name = Customers.name");
+			stmt.setString(5, sendLocation());
 			stmt.executeUpdate();
-			
-			if(getIDofLastInsertedRow() > -1) {
-				//Do subtract of stored goods - then update.
+			palletID = getIDofLastInsertedRow();
+			if(palletID > -1) {
+				//Do subtract of stored goods - then update. 
+				subtractFromStorage(cookie);
 			}
-			
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return Jsonizer.anythingToJson("error", "status");
 		}
 		
-		return "{}";
+		return Jsonizer.anythingToJson(palletID, "id");
 	}
 	
 	private boolean cookieExists(String cookieName) {
-		String Q = "SELECT * FROM Cookies WHERE name = ?";
+		String Q = "SELECT * FROM Cookies WHERE cookieName = ?";
 		
 		try(PreparedStatement stmt = conn.prepareStatement(Q)) {
 			stmt.setString(1, cookieName);
@@ -145,16 +149,17 @@ public class Database {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			
 		}
 		return false;
 	}
 	
 	private int createOrder() {
 		
-		String Q = "INSERT INTO Order(name, placedDate) VALUES(?,?)";
+		String Q = "INSERT INTO Orders(name, placedDate) VALUES(?,?)";
 		try (PreparedStatement stmt = conn.prepareStatement(Q)) {
 			
-			ResultSet rs = stmt.executeQuery();
+			
 			stmt.setString(1, getCustomer());
 			stmt.setString(2, "CURRENT_TIMESTAMP");
 			stmt.executeUpdate();
@@ -163,9 +168,6 @@ public class Database {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
 		
 		return getIDofLastInsertedRow();
 	}
@@ -200,15 +202,101 @@ public class Database {
 			}
 			rs.close();
 			
-		} catch (SQLException ero) {
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
-			ero.printStackTrace();
+			e.printStackTrace();
 		}
 		
 		return null;
 	}
 	
-	private void subtractFromstorage(String cookieName) {
+	private void subtractFromStorage(String cookieName) {
+		//Hashmap to order an ingredient with respective amount
+		HashMap<String, Integer> IngAmount = new HashMap<String, Integer>(); 
 		
+		//In order to set multiple transactions during one commit
+		//We must set autocommit = false
+		try {
+			conn.setAutoCommit(false);
+			//Get all ingredients used in the making of the cookie aswell as 
+			//the stored amount of each ingredient
+			String Q = "SELECT Ingredients.name, amountStored FROM Ingredients \n" + 
+					"INNER JOIN ContainsIngredients \n" + 
+					"ON ContainsIngredients.ingredient = Ingredients.name \n" + 
+					"WHERE cookieName = ?;";
+			PreparedStatement stmt = conn.prepareStatement(Q);
+			stmt.setString(1, cookieName);
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				//Map them...
+				IngAmount.put(rs.getString("name"), rs.getInt("amountStored"));
+			}
+			stmt.close();
+			//Get the amount that is used per ingredient...
+			Q = "SELECT ingredient, amount FROM ContainsIngredients WHERE cookieName = ?";
+			
+			stmt = conn.prepareStatement(Q);
+			//With cookiename as param
+			stmt.setString(1, cookieName);
+			rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				
+				String ingredient = rs.getString("ingredient");
+				int amount = rs.getInt("amount");
+				//Update storage by subtracting
+				IngAmount.put(ingredient, IngAmount.get(ingredient) - rs.getInt("amount") * 54);
+			}
+			
+			for (Map.Entry<String, Integer> entry : IngAmount.entrySet()) { // Update each ingredient
+
+				Q = "UPDATE Ingredients SET amountStored = ? WHERE name = ?"; //name = ingredient
+				stmt = conn.prepareStatement(Q);
+				stmt.setInt(1, entry.getValue());
+				stmt.setString(2, entry.getKey());
+				stmt.executeUpdate();
+				stmt.close();
+
+			}
+
+			conn.commit();
+			
+		
+		} catch(Exception e) {
+			
+				try {
+					conn.rollback();
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+				e.printStackTrace();
+		}
+
+		try {
+			conn.setAutoCommit(true);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private String sendLocation() {
+		
+		String Q = "SELECT address FROM Orders, Customers WHERE Orders.name = Customers.name";
+		
+		try(PreparedStatement stmt = conn.prepareStatement(Q)){
+			
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				return rs.getString("address");
+			}
+			rs.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
